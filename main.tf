74b00f557991b58677f6558a444c9faee906f8ff
BenKnust <knust.benjamin@gmail.com>
2023-11-03 21:59:30 +0100

    added vpc config

--- main.tf
+++ main.tf
@@ -47,7 +47,7 @@ resource "aws_instance" "jumphost" {
   instance_type = var.instance_type
 
   #vpc_security_group_ids = [aws_security_group.blog.id]
-  #vpc_security_group_ids = [module.blog_sg.security_group_id]
+  vpc_security_group_ids = [module.jumphost_sg.security_group_id]
   key_name = "centos9_jump_host"
 
   tags = {
@@ -103,6 +103,17 @@ module "blog_sg" {
   name = "blog_new"
   vpc_id = data.aws_vpc.default.id
   ingress_rules = ["http-80-tcp","https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
+}
+
+module "jumphost_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.1.0"
+  name = "jumphost"
+  vpc_id = data.aws_vpc.default.id
+  ingress_rules = ["ssh-22-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
   egress_rules = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
