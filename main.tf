a109fcce9fa731fb6ee8d3671675badf21e1074d
BenKnust <knust.benjamin@gmail.com>
2023-11-03 19:10:41 +0100

    module for security group

--- main.tf
+++ main.tf
@@ -25,7 +25,8 @@ resource "aws_instance" "Blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [aws_security_group.blog.id]
+  #vpc_security_group_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
     Name = "Blog"
@@ -61,6 +62,16 @@ resource "aws_db_instance" "example" {
   }
 }
 
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.1.0"
+  name = "blog_new"
+  vpc_id = data.aws_vpc.default.id
+  ingress_rules = ["http-80-tcp","https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
+}
 
 resource "aws_security_group" "blog" {
   name        = "blog"
